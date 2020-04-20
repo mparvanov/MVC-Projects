23c604408614ad8377fba45486e005d91860b51f
Mihail Parvanov <mihail.parvanov@progress.com>
2020-04-20 18:45:31 +0300

    Add editor value post encoded

---
+++ EditorPostValueEncoded/TelerikMvcApp1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelerikMvcApp1.Models;
+
+namespace TelerikMvcApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(PersonModel person)
+        {
+            int personId = person.PersonId;
+            string name = person.Name;
+            string gender = person.Gender;
+            string city = person.City;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EditorPostValueEncoded/TelerikMvcApp1/Models/LogonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TelerikMvcApp1.Models
+{
+    public class LogonModel
+    {
+		[Required]
+		[Display(Name = "User name")]
+		public string UserName { get; set; }
+
+		
+	}
+}
